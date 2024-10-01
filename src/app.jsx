08b842ba2d9ccf1158cf183d1d0090506a9c8a4f
Gustavo Oly <gustavooly.dev@gmail.com>
2024-09-30 23:56:22 -0300

    feat: src file

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+import "./main.css";
+
+ReactDom.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <div className="h-dvh bg-black"></div>
+  </React.StrictMode>,
+);
