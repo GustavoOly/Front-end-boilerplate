6d3e2752e9e0632d314358e85e0f3ae0c8702cb6
Gustavo Oly <gustavooly.dev@gmail.com>
2024-10-01 13:23:29 -0300

    feat(app.jsx): use strict mode

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,13 @@
+"use strict";
+
 import React from "react";
 import ReactDom from "react-dom/client";
 import "./main.css";
 
 ReactDom.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div className="h-dvh bg-black"></div>
+    <div className="h-dvh dark:bg-black dark:text-white">
+      <h1>Olá mundo</h1>
+    </div>
   </React.StrictMode>,
 );
